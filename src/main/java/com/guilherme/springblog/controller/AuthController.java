8becf745c9a817d8f95f116d6879bdfcf9a0cacc
guilhermeozana <guilhermeozana@hotmail.com>
2021-05-13 14:27:23 -0300

    implemented cors

--- src/main/java/com/guilherme/springblog/controller/AuthController.java
+++ src/main/java/com/guilherme/springblog/controller/AuthController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200",maxAge = 3600L) 
 public class AuthController {
     private final AuthService authService;
 
